0a546008a40a1f064e531657670c12f9e58a6425
Martina Beauvais <marbeauvais17@gmail.com>
2023-03-22 16:34:21 -0300

    Added env variable

--- src/config.js
+++ src/config.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res, next) =>{
     res.send('Escuchando con Railway...')
 })
+app.get('/env', (req, res, next) => {
+    res.send(`Solicitando el entorno: ${process.env.VICIOUSCOFFE}`)
+})
 
 // LISTEN
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
